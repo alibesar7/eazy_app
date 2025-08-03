df1008db79ad49097c4b2e44f96f00816035bc03
alibesar7 <alibesar28@gmail.com>
2025-08-03 14:16:34 +0300

    initial

---
+++ lib/ui/Custom_Widgets/Custom_Button.dart
@@ -0,0 +1,28 @@
+import 'package:esay_app/utils/appColors.dart';
+import 'package:esay_app/utils/appStyles.dart';
+import 'package:flutter/material.dart';
+
+
+class CustomElevatedButton extends StatelessWidget {
+  final VoidCallback onButtonClick;
+  final String text;
+
+  const CustomElevatedButton({
+    Key? key,
+    required this.onButtonClick,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),),
+      onPressed: onButtonClick,
+      child: Text(text,style:  AppTextStyles.caption,),
+    );
+  }
+}
